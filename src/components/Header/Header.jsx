bf5f61a530bd7a2f384a6ed3c3c95adf3a298db9
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-09-25 21:30:18 +0200

    Started the project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo'>
+        <h1>Eat Well</h1>
+      </div>
+      <div className='pages'>
+        <h3>Recipes</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
