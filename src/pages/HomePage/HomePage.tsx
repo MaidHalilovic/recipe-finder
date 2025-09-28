f8e37c113e6fa6df56ac1ef57fdf47f887284eaa
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-09-28 21:00:31 +0200

    Making homePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./homePage.css";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
-  //5-6  random meals koji se refreshaju svaki put na reftesh stranice
-  //search bar ce se napraviti
-  //categories
-  //popular meals
-  //areas
+  return (
+    <div className='homePage'>
+      <div></div>
+    </div>
+  );
 };
 
 export default HomePage;
+
+//5-6  random meals koji se refreshaju svaki put na reftesh stranice
+//search bar ce se napraviti
+//categories
+//popular meals
+//areas
